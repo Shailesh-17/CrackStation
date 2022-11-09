2df6b4d9653bcaecb8e2c7a80b4659c97222f83c
sem <sem@gmail.com>
2022-11-09 00:36:24 +0000

    Fixed the bug

--- Sources/CrackStation/CrackStation.swift
+++ Sources/CrackStation/CrackStation.swift
@@ -2,7 +2,7 @@ import Foundation
 import Crypto
 
 public class CrackStation: Decrypter{
-  let validChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  let validChar =  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
   public required init(){}
 
@@ -15,19 +15,21 @@ public class CrackStation: Decrypter{
   }
 
   public func decrypt(shaHash: String) -> String?{
-      var dict: [String: String] = [:]
+      var dict1: [String: String] = [:]
+      var dict2: [String: String] = [:]
       for i in validChar{
-      dict[encryptDic(i)] = i
-      for j in validChar{
-        let new_str = i + j
-        dict[encryptDic(new_str)] = new_str
+      dict1[encryptDic(i)] = i
       }
+      for i in validChar{
+        for j in validChar{
+            let new_str = i + j
+            dict2[encryptDic(new_str)] = new_str
+            }
+        }
 
+        dict1.merge(dict2){(first, _) in first}
+        let n = dict1[shaHash]
+        return n
     }
-    print(dict)
-    let n = dict[shaHash]
-    return n
   }
 
-
-}
